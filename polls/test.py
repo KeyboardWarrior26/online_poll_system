f3d8b6a48dc5f32d5b213b6e64bdedf78117b6e3
Thamsanqa <thamzafaniso@gmail.com>
2025-07-22 22:31:49 +0200

    Following CI/CD pipeline

---
+++ polls/test.py
@@ -0,0 +1,67 @@
+from django.urls import reverse
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question, Choice
+import datetime
+
+def create_question(question_text, days):
+        """
+        Create a question with the given `question_text` and published the
+        given number of `days` offset to now (negative for past, positive for future).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        future_time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        recent_time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=recent_time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_questions(self):
+        """If no questions exist, an appropriate message is displayed."""
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """Questions with pub_date in the past are displayed on the index page."""
+        question = create_question(question_text="Past question.", days=-10)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_future_question(self):
+        """Questions with a pub_date in the future are not displayed on the index page."""
+        create_question(question_text="Future question.", days=10)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """The detail view of a question with a future pub_date returns 404."""
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """The detail view of a question with a past pub_date displays the question's text."""
+        past_question = create_question(question_text='Past question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+from django.db.models import Sum
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    total_votes = question.choice_set.aggregate(Sum('votes'))['votes__sum'] or 0
+
+    # Add percentage manually to each choice
+    for choice in question.choice_set.all():
+        if total_votes > 0:
+            choice.percentage = round((choice.votes / total_votes) * 100, 1)
+        else:
+            choice.percentage = 0
+
+    return render(request, 'polls/results.html', {
+        'question': question,
+        'total_votes': total_votes
+    })
